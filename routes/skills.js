96dedb393b9c316bd2e465cc2b22a5526ae26f2e
EdASalazar <edasalazar123@gmail.com>
2023-01-31 15:55:48 -0800

    Added skillsCtrl

--- routes/skills.js
+++ routes/skills.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var skillsCtrl = require('../controllers/skills');
+
 /* GET users listing. */
 
 router.get('/', skillsCtrl.index);
