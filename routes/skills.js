5cb6f8e2271883933f269f16909ef6bd0191e2b9
EdASalazar <edasalazar123@gmail.com>
2023-01-31 16:14:59 -0800

    Got most of it working. Still need to get the id look up to work.

--- routes/skills.js
+++ routes/skills.js
@@ -1,11 +1,13 @@
 var express = require('express');
+//factory function. Used to create something
 var router = express.Router();
-
 var skillsCtrl = require('../controllers/skills');
 
 /* GET users listing. */
-
+//all actual paths start with "/todos"
+//GET /todos
 router.get('/', skillsCtrl.index);
+// GET /todos/:id
 router.get('/:id', skillsCtrl.show);
 
 module.exports = router;
